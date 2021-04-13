2dc16f86e3bef84f081e1dc1bf9ebae2d3d663d9
glezzz <alexglezk@gmail.com>
2021-04-13 23:20:13 +0200

    dagger hilt setup, fixed bugs

--- app/src/main/java/com/myproject/spotifyclone/MainActivity.kt
+++ app/src/main/java/com/myproject/spotifyclone/MainActivity.kt
@@ -2,8 +2,17 @@ package com.myproject.spotifyclone
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.RequestManager
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+// annotation used every time something is injected into any Android component
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var glide: RequestManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
