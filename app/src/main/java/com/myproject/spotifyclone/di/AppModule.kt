9ec3b9f113d79b436d276a613943bab098bc521b
glezzz <alexglezk@gmail.com>
2021-04-13 21:26:48 +0200

    working on modules

--- app/src/main/java/com/myproject/spotifyclone/di/AppModule.kt
+++ app/src/main/java/com/myproject/spotifyclone/di/AppModule.kt
@@ -1,12 +1,34 @@
 package com.myproject.spotifyclone.di
 
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.myproject.spotifyclone.R
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-
+    // tell Dagger the dependencies we need
+    // Dagger hilt will call this function behind the scenes for us
+    // makes sure we only have one instance of the Glide instance
+    @Singleton
+    @Provides
+    fun provideGlideInstance(
+        @ApplicationContext context: Context
+    ) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            // if image is not fully loaded, display this one
+            .placeholder(R.drawable.ic_image)
+            // same for error
+            .error(R.drawable.ic_image)
+            // makes sure images are cached with Glide
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
 }
