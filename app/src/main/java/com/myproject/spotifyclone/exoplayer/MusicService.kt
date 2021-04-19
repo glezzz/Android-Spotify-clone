9a1977dfc49347e3e411d2442e3ea8176da3d832
glezzz <alexglezk@gmail.com>
2021-04-19 18:11:44 +0200

    music service setup

---
+++ app/src/main/java/com/myproject/spotifyclone/exoplayer/MusicService.kt
@@ -0,0 +1,81 @@
+package com.myproject.spotifyclone.exoplayer
+
+import android.app.PendingIntent
+import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.media.MediaBrowserServiceCompat
+import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ext.mediasession.MediaSessionConnector
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import javax.inject.Inject
+
+private const val SERVICE_TAG = "MusicService"
+
+@AndroidEntryPoint
+class MusicService : MediaBrowserServiceCompat() {
+
+
+    @Inject
+    lateinit var dataSourceFactory: DefaultDataSourceFactory
+
+    @Inject
+    lateinit var exoPlayer: SimpleExoPlayer
+
+    private val serviceJob = Job()
+    private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
+
+    private lateinit var mediaSession: MediaSessionCompat
+    private lateinit var mediaSessionConnector: MediaSessionConnector
+
+    override fun onCreate() {
+        super.onCreate()
+        val activityIntent = packageManager?.getLaunchIntentForPackage(packageName)?.let {
+            PendingIntent.getActivity(this, 0, it, 0)
+        }
+
+        mediaSession = MediaSessionCompat(this, SERVICE_TAG).apply {
+            setSessionActivity(activityIntent)
+            isActive = true
+        }
+
+        sessionToken = mediaSession.sessionToken
+
+        // Initialize mediaSessionConnector
+        mediaSessionConnector = MediaSessionConnector(mediaSession)
+        // we set player to exoPlayer that is injected above, created in ServiceModule
+        mediaSessionConnector.setPlayer(exoPlayer)
+    }
+
+    /**
+     * Coroutines are cancelled when server dies
+     */
+    override fun onDestroy() {
+        super.onDestroy()
+        serviceScope.cancel()
+    }
+
+    override fun onGetRoot(
+        clientPackageName: String,
+        clientUid: Int,
+        rootHints: Bundle?
+    ): BrowserRoot? {
+        TODO("Not yet implemented")
+    }
+
+    /**
+     *
+     * @param parentId ID to get the list of songs of
+     */
+    override fun onLoadChildren(
+        parentId: String,
+        result: Result<MutableList<MediaBrowserCompat.MediaItem>>
+    ) {
+        TODO("Not yet implemented")
+    }
+}
