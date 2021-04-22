2521eee14cad04dfb46078b60376b17e77b399b7
glezzz <alexglezk@gmail.com>
2021-04-22 18:03:26 +0200

    setting up music notification

--- app/src/main/java/com/myproject/spotifyclone/other/Constansts.kt
+++ app/src/main/java/com/myproject/spotifyclone/other/Constansts.kt
@@ -3,4 +3,7 @@ package com.myproject.spotifyclone.other
 object Constansts {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
