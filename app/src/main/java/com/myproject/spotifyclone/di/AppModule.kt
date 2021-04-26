980accf013e43a11b3517d3723301c1d87162537
glezzz <alexglezk@gmail.com>
2021-04-26 14:21:51 +0200

    mainViewModel setup

--- app/src/main/java/com/myproject/spotifyclone/di/AppModule.kt
+++ app/src/main/java/com/myproject/spotifyclone/di/AppModule.kt
@@ -5,6 +5,7 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.myproject.spotifyclone.R
+import com.myproject.spotifyclone.exoplayer.MusicServiceConnection
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -15,6 +16,13 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideMusicServiceConnection(
+        @ApplicationContext context: Context
+    ) = MusicServiceConnection(context)
+
     // tell Dagger the dependencies we need
     // Dagger hilt will call this function behind the scenes for us
     // makes sure we only have one instance of the Glide instance
--- app/src/main/java/com/myproject/spotifyclone/ui/MainActivity.kt
+++ app/src/main/java/com/myproject/spotifyclone/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.myproject.spotifyclone
+package com.myproject.spotifyclone.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.RequestManager
+import com.myproject.spotifyclone.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
